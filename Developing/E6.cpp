c9624c7eaa409bb4db0fd17094f754c986698057
Felipe Gemmal <lipegemmal@hotmail.com>
2018-11-23 08:05:35 -0200

    batata

--- Developing/E6.cpp
+++ Developing/E6.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main(){
-    vector<unsigned int> elem;
+    vector<int> elem;
     int size;
     int input_elem;
     int stack_size =0;
@@ -22,29 +22,40 @@ int main(){
     sort(elem.begin(), elem.end());
     reverse(elem.begin(), elem.end());
 
-    for(size_t i=0 ; i< elem.size(); i++){
-        cout << elem[i] <<" ";
-    }
-    cout << "\n";
+   
 
     while(!elem.empty()){
 
         max_ring_size = __INT_MAX__;
         stack_size++;
+        
+        /*
+        cout << "Stack: "<<stack_size<<"\n";
 
-        for(size_t i=0 ; i< elem.size() ; i++){
+        for (size_t i = 0; i < elem.size(); i++){
+            cout << elem[i] << " ";
+        }
+        cout <<"\n";
+        */
+        
+        for(size_t i = 0; i < elem.size(); i++){
+
+           // cout << "Comparing: "<< elem[i] << " e "<< max_ring_size<< "\n";
             if(elem[i] <= max_ring_size ){
-                
-                cout << "removing " << elem[i] << " in stack " << stack_size <<"\n";
 
                 max_ring_size = elem[i] -2;
+               // cout << "Entrei \n";
+                //cout << "removing " << elem[i] << " in stack " << stack_size << " max ring size " << max_ring_size << "\n";
                 elem.erase(elem.begin()+i);
+                i--;
             }
         }
+        
+    
        
     }
 
-    cout << "The stack is " << stack_size << " rings tall\n";
+    cout << "The stack is " << stack_size << " rings tall.\n";
 
     return 0;
 }
